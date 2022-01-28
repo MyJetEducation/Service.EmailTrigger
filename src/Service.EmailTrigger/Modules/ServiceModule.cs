1548bfa085188cf52b465e0a8e85ce061ab434b1
dmitriy.b <dmitriy.b@smplt.net>
2022-01-28 18:41:24 +0500

    Subscribers fixed

--- src/Service.EmailTrigger/Modules/ServiceModule.cs
+++ src/Service.EmailTrigger/Modules/ServiceModule.cs
@@ -3,6 +3,7 @@ using MyJetWallet.Sdk.ServiceBus;
 using MyServiceBus.Abstractions;
 using MyServiceBus.TcpClient;
 using Service.EmailSender.Client;
+using Service.EmailTrigger.Jobs;
 using Service.ServiceBus.Models;
 
 namespace Service.EmailTrigger.Modules
@@ -18,6 +19,9 @@ namespace Service.EmailTrigger.Modules
 			MyServiceBusTcpClient serviceBusClient = builder.RegisterMyServiceBusTcpClient(Program.ReloadedSettings(e => e.ServiceBusReader), Program.LogFactory);
 			builder.RegisterMyServiceBusSubscriberBatch<RecoveryInfoServiceBusModel>(serviceBusClient, RecoveryInfoServiceBusModel.TopicName, QueueName, TopicQueueType.Permanent);
 			builder.RegisterMyServiceBusSubscriberBatch<RegistrationInfoServiceBusModel>(serviceBusClient, RegistrationInfoServiceBusModel.TopicName, QueueName, TopicQueueType.Permanent);
+
+			builder.RegisterType<RecoveryInfoEmailNotificator>().AutoActivate().SingleInstance();
+			builder.RegisterType<RegistrationInfoEmailNotificator>().AutoActivate().SingleInstance();
 		}
 	}
 }
