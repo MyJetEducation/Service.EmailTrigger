0930f4ab7af299342c5e4187d9b171ea30c0de7e
dmitriy.b <dmitriy.b@smplt.net>
2022-02-23 13:00:35 +0500

    FIN360PMVP-170: DI fixed

--- src/Service.EmailTrigger/Modules/ServiceModule.cs
+++ src/Service.EmailTrigger/Modules/ServiceModule.cs
@@ -19,9 +19,11 @@ namespace Service.EmailTrigger.Modules
 			MyServiceBusTcpClient serviceBusClient = builder.RegisterMyServiceBusTcpClient(Program.ReloadedSettings(e => e.ServiceBusReader), Program.LogFactory);
 			builder.RegisterMyServiceBusSubscriberBatch<RecoveryInfoServiceBusModel>(serviceBusClient, RecoveryInfoServiceBusModel.TopicName, QueueName, TopicQueueType.Permanent);
 			builder.RegisterMyServiceBusSubscriberBatch<RegistrationInfoServiceBusModel>(serviceBusClient, RegistrationInfoServiceBusModel.TopicName, QueueName, TopicQueueType.Permanent);
+			builder.RegisterMyServiceBusSubscriberBatch<ChangeEmailServiceBusModel>(serviceBusClient, ChangeEmailServiceBusModel.TopicName, QueueName, TopicQueueType.Permanent);
 
 			builder.RegisterType<RecoveryInfoEmailNotificator>().AutoActivate().SingleInstance();
 			builder.RegisterType<RegistrationInfoEmailNotificator>().AutoActivate().SingleInstance();
+			builder.RegisterType<ChangeEmailServiceBusModel>().AutoActivate().SingleInstance();
 		}
 	}
 }
